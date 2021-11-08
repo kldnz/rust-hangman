16d82d17d0bed747f0750671260043fd940eed20
kldnz <vytautas.balta@gmail.com>
2021-11-08 13:59:25 +0200

    Added progress bar with hidden and revelead funcionallity, splitting word by char and working with every char

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Letter {
+    character: char,
+    revealed: bool
+}
+
 fn main() {
     let selected_word = select_word();
+    let mut letters = create_letters(&selected_word);
 
-    println!("The selected word was {}", selected_word);
+    display_progress(&letters);
+    println!("Selected word was {}", selected_word);
 }
 
 fn select_word() -> String {
@@ -26,4 +33,38 @@ fn select_word() -> String {
     let random_index = rand::thread_rng().gen_range(0, available_words.len());
 
     return String::from(available_words[random_index]);
+}
+
+fn create_letters(word: &String) -> Vec<Letter> {
+    // Create empty vector
+    let mut letters: Vec<Letter> = Vec::new();
+
+    // Wrap each character in a Letter Struct
+    for c in word.chars() {
+        letters.push(Letter {
+            character: c,
+            revealed: false
+        });
+    }
+
+    return letters;
+}
+
+fn display_progress(letters: &Vec<Letter>) {
+    let mut display_string = String::from("Progress:"); // Example: Progress: _ a _ a _ y
+
+    // Display appropriate character (letter or _) for each letter
+    for letter in letters {
+        display_string.push(' ');
+
+        if letter.revealed {
+            display_string.push(letter.character);
+        } else{
+            display_string.push('_');
+        }
+
+        display_string.push(' ');
+    }
+
+    println!("{}", display_string)
 }
